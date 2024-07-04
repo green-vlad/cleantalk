28509ff239995e8541e11be6aa8023711ddfb6e7
Andrey Kim <green.vlad@gmail.com>
2024-07-04 16:21:11 +0900

    Added CORS middleware to backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\EmployeeController;
+use App\Http\Middleware\ApiCors;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/', [EmployeeController::class, 'index']);
+Route::get('/', [EmployeeController::class, 'index'])->middleware(ApiCors::class);
