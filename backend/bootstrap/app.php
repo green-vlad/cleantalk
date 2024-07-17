5e712e25ac0cbcaf56dbde0b0e4a9d9bc1576720
Andrey Kim <green.vlad@gmail.com>
2024-07-17 10:14:57 +0900

    cors fix

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ApiCors;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(ApiCors::class);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
