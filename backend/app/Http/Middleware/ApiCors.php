3aa66e1ebfcf817038b7e55f102597136148e0dc
Andrey Kim <green.vlad@gmail.com>
2024-07-17 10:59:04 +0900

    cors error fix

--- backend/app/Http/Middleware/ApiCors.php
+++ backend/app/Http/Middleware/ApiCors.php
@@ -17,10 +17,8 @@ class ApiCors
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
-            ->header('Accept', 'application/json');;
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type,Accept,Authorization,X-Requested-With');
         return $response;
     }
 }
