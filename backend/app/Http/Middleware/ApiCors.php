1025d135b77266d8556ac117f1d3878111f69361
Andrey Kim <green.vlad@gmail.com>
2024-07-17 09:57:20 +0900

    cors fix

--- backend/app/Http/Middleware/ApiCors.php
+++ backend/app/Http/Middleware/ApiCors.php
@@ -16,7 +16,11 @@ class ApiCors
     public function handle(Request $request, Closure $next): Response
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', '*')
+            ->header('Access-Control-Allow-Credentials', true)
+            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+            ->header('Accept', 'application/json');;
         return $response;
     }
 }
