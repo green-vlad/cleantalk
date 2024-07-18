903e18ee528342c41e2ac1fdb336642d7855e4ad
Andrey Kim <green.vlad@gmail.com>
2024-07-18 11:35:19 +0900

    fix cors errors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => [':authority:', ':method:', ':path:', ':scheme:'],
 
     'exposed_headers' => [],
 
